1338a9215386c6d20b19bfd172c41236c1148d02
GugaPixel360 <gmadureira198@gmail.com>
2026-09-14 14:14:25 -0300

    arquivo sql do trabalho

---
+++ DB-C#.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS controle_despesas;
+
+USE controle_despesas;
+
+CREATE TABLE IF NOT EXISTS despesas (
+id_dps INT AUTO_INCREMENT PRIMARY KEY,
+descricao_dps VARCHAR (300),
+valor_dps DECIMAL (10,2),
+categoria_dps VARCHAR (150),
+data_dps DATETIME
+
+);
+
+
