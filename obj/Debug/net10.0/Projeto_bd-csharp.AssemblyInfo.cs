424a683d5d115d7f9d41e102d942f9e12e462574
GugaPixel360 <gmadureira198@gmail.com>
2026-09-17 14:14:38 -0300

    cache

--- obj/Debug/net10.0/Projeto_bd-csharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projeto_bd-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_bd-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4226ede39c0c0507a34c3ba2c7052b7747b4cdb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418fe29cf23ada69f05463e8b3680ea942163271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_bd-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_bd-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
